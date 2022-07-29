da5616848069016ddd9157967c6ece826e079713
WHIZENx <nattapatpin@gmail.com>
2022-07-29 18:16:26 +0700

    - add new resouces pokemon go - fix error page

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import error from '../../assets/error.jpg';
 
 import './Error.css';
 
@@ -14,14 +13,14 @@ const Error = () => {
 
     return (
         <div className='d-block position-relative'>
-            <div className="error-img"><img alt="error-img" src={error}/></div>
+            <div className="error-img"><div className='img'></div></div>
             <div className="error-desc">
                 <div className='desc'>
                     <h1 className='desc-head'><b>404</b></h1>
                     <h1>Page Not Found</h1>
                     <span>It looks like nothing was found at <p style={{color: 'yellow'}}>{thisLocation.pathname}</p></span>
-                    <span>Maybe try one of the links in the menu or press Go home to go to the home page.</span>
-                    <div style={{marginTop: 15}}><Link className='btn btn-danger' to="/">Go home</Link></div>
+                    <span>Maybe try one of the links in the menu or press Back to Home to go to the home page.</span>
+                    <div style={{marginTop: 15}}><Link className='btn btn-danger' to="/">Back to Home</Link></div>
                 </div>
             </div>
         </div>
