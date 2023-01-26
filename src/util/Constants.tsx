51d19c4ff3b6fe76e928d8d31a321c3655cdaace
WHIZENx <nattapatpin@gmail.com>
2023-01-26 17:20:41 +0700

    add stats ranking page

--- src/util/Constants.tsx
+++ src/util/Constants.tsx
@@ -198,3 +198,26 @@ export const leaguesTeamBattle = [
     logo: APIService.getPokeLeague('master_league'),
   },
 ];
+
+export const genRoman = (gen: number) => {
+  switch (gen) {
+    case 1:
+      return 'I';
+    case 2:
+      return 'II';
+    case 3:
+      return 'III';
+    case 4:
+      return 'IV';
+    case 5:
+      return 'V';
+    case 6:
+      return 'VI';
+    case 7:
+      return 'VII';
+    case 8:
+      return 'VIII';
+    default:
+      return '';
+  }
+};
