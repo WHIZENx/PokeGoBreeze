4671aa845e82b3a5411684d56fd79b2fe8b844c9
Nattapat Pinta <47101447+WHIZENx@users.noreply.github.com>
2025-05-11 18:14:07 +0700

    change package build config

---
+++ vercel.build.js
@@ -0,0 +1,17 @@
+const { execSync } = require('child_process');
+
+const branch = process.env.VERCEL_GIT_COMMIT_REF;
+console.log(`🪵 Branch Detected: ${branch}`);
+
+try {
+  if (branch === 'develop' || branch === 'stage') {
+    execSync('npm run develop', { stdio: 'inherit' });
+  } else if (branch === 'main') {
+    execSync('npm run deploy', { stdio: 'inherit' });
+  } else {
+    throw new Error(`Unsupported branch: ${branch}`);
+  }
+} catch (err) {
+  console.error('❌ Build failed:', err);
+  process.exit(1);
+}
